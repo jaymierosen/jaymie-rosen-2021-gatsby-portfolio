beddf594628279706b43ccb2949d9f1336aa5925
jaymierosen <rosen.jaymie@gmail.com>
2021-10-29 23:22:14 -0400

    adding meta data title; adding new favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starterw`,
         start_url: `/`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/smiley.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-gatsby-cloud`,
--- src/components/seo.js
+++ src/components/seo.js
@@ -1,81 +1,56 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { Helmet } from "react-helmet"
-import { useLocation } from "@reach/router"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { useStaticQuery, graphql } from 'gatsby';
 
-const Seo = ({ title, description, image, article }) => {
-  const { pathname } = useLocation()
-  const { site } = useStaticQuery(query)
-
-  const {
-    titleTemplate,
-    defaultDescription,
-    siteUrl,
-    defaultImage,
-    twitterUsername,
-  } = site.siteMetadata
-
-  const seo = {
-    title: "Jaymie Rosen",
-    defaultDescription: defaultDescription,
-    description: description,
-    image: `${siteUrl}${image || defaultImage}`,
-    url: `${siteUrl}${pathname}`,
-  }
-
-  return (
-    <Helmet title={seo.title} titleTemplate={titleTemplate}>
-      <title>{seo.title}</title>
-      <meta name="description" content={seo.description} />
-      <meta name="image" content={seo.image} />
-      {seo.url && <meta property="og:url" content={seo.url} />}
-      {(article ? true : null) && <meta property="og:type" content="article" />}
-      {seo.title && <meta property="og:title" content={seo.title} />}
-      {seo.description && (
-        <meta property="og:description" content={seo.description} />
-      )}
-      {seo.image && <meta property="og:image" content={seo.image} />}
-      <meta name="twitter:card" content="summary_large_image" />
-      {twitterUsername && (
-        <meta name="twitter:creator" content={twitterUsername} />
-      )}
-      {seo.title && <meta name="twitter:title" content={seo.title} />}
-      {seo.description && (
-        <meta name="twitter:description" content={seo.description} />
-      )}
-      {seo.image && <meta name="twitter:image" content={seo.image} />}
-    </Helmet>
-  )
-}
-
-export default Seo;
-
-Seo.propTypes = {
-  title: PropTypes.string,
-  description: PropTypes.string,
-  image: PropTypes.string,
-  article: PropTypes.bool,
-}
-
-Seo.defaultProps = {
-  title: "Jaymie Rosen",
-  description: null,
-  image: null,
-  article: false,
-}
-
-const query = graphql`
-  query Seo {
-    site {
-      siteMetadata {
-        defaultTitle: title
-        titleTemplate
-        defaultDescription: description
-        siteUrl: url
-        defaultImage: image
-        twitterUsername
+const Seo = ({ post }) => {
+  const data = useStaticQuery(graphql`
+    {
+      site {
+        siteMetadata {
+          title
+          description
+          url
+        }
       }
     }
+  `);
+
+  const defaults = data.site.siteMetadata;
+
+  if (defaults.url === '' && typeof window !== 'undefined') {
+    defaults.url = window.location.origin;
   }
-`
+
+  if (defaults.url === '') {
+    console.error('Please set a Url in your site metadata!');
+    return null;
+  }
+
+  const title = post.title || defaults.title;
+  const description = post.description || defaults.description;
+  const url = new URL(post.path || '', defaults.url);
+  const image = post.image ? new URL(post.image, defaults.url) : false;
+
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <link rel="canonical" href={url} />
+      <meta name="description" content={description} />
+      {image && <meta name="image" content={image} />}
+
+      <meta property="og:url" content={url} />
+      <meta property="og:type" content="article" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      {image && <meta property="og:image" content={image} />}
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:creator" content={post.author.twitter} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      {image && <meta name="twitter:image" content={image} />}
+    </Helmet>
+  );
+};
+
+export default Seo;
